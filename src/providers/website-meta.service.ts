6ef329f2f5dbf1f604902434c07e9c87e975859f
Adeeb Shah <adeebshah4@gmail.com>
2026-05-14 20:15:14 +0530

    Feat: Social meand links to provider

--- src/providers/website-meta.service.ts
+++ src/providers/website-meta.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { StorageService } from '../storage/storage.service';
 
-interface WebsiteMeta {
+export interface WebsiteMeta {
   logoUrl: string | null;
   title: string | null;
 }
